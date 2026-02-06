1137cac74080e8c00017551e8e8c344464f80815
Mathis-Desbonnet <mathis.desbonnet@gmail.com>
2026-02-06 18:04:26 +0100

    Added console.log when player click

--- src/app/gameAdmin/page.js
+++ src/app/gameAdmin/page.js
@@ -8,116 +8,134 @@ import { Button } from "@/components/ui/button";
 let socket;
 
 export default function AdminPanel() {
-	const [playersJoined, setPlayersJoined] = useState([]);
-	const [playersAnswered, setPlayersAnswered] = useState([]);
-	const [userInfo, setUserInfo] = useState(null);
-	const router = useRouter();
-	useEffect(() => {
-		async function fetchData() {
-			const response = await getUserInfo();
-			setUserInfo(response);
-			if (!response.admin) router.push("/");
-		}
-		fetchData();
-	}, []);
+  const [playersJoined, setPlayersJoined] = useState([]);
+  const [playersAnswered, setPlayersAnswered] = useState([]);
+  const [userInfo, setUserInfo] = useState(null);
+  const router = useRouter();
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getUserInfo();
+      setUserInfo(response);
+      if (!response.admin) router.push("/");
+    }
+    fetchData();
+  }, []);
 
-	useEffect(() => {
-		socket = io(process.env.API_URI);
+  useEffect(() => {
+    socket = io(process.env.API_URI);
 
-		socket.on("update_players_joined", (data) => {
-			if (playersJoined.findIndex((element) => element.username == data.username) != -1 || playersAnswered.findIndex((element) => element.username == data.username) != -1) return;
-			setPlayersJoined([...playersJoined, data]);
-		});
+    socket.on("update_players_joined", (data) => {
+      if (
+        playersJoined.findIndex(
+          (element) => element.username == data.username,
+        ) != -1 ||
+        playersAnswered.findIndex(
+          (element) => element.username == data.username,
+        ) != -1
+      )
+        return;
+      setPlayersJoined([...playersJoined, data]);
+    });
 
-		socket.on("update_buzz_list", (data) => {
-			const players = playersJoined.slice();
-			players.splice(
-				players.findIndex((element) => element.username == data.username),
-				1,
-			);
-			setPlayersJoined(players);
-			function sort_by_key(array, key, way) {
-				//way : ASC = 1; DESC = -1
-				return array.sort(function (a, b) {
-					var x = a[key];
-					var y = b[key];
-					return x < y ? -1 * way : x > y ? 1 * way : 0;
-				});
-			}
-			setPlayersAnswered([...playersAnswered, data]);
-		});
+    socket.on("update_buzz_list", (data) => {
+      const players = playersJoined.slice();
+      console.log(data);
+      players.splice(
+        players.findIndex((element) => element.username == data.username),
+        1,
+      );
+      setPlayersJoined(players);
+      setPlayersAnswered([...playersAnswered, data]);
+    });
 
-		return () => {
-			socket.disconnect();
-		};
-	}, [playersJoined]);
+    return () => {
+      socket.disconnect();
+    };
+  }, [playersJoined]);
 
-	const handleRemovePlayers = () => {
-		setPlayersJoined([]);
-		setPlayersAnswered([]);
-	};
+  const handleRemovePlayers = () => {
+    setPlayersJoined([]);
+    setPlayersAnswered([]);
+  };
 
-	const allowAnswer = () => {
-		socket = io(process.env.API_URI);
-		socket.emit("set_player_buzz_state", {
-			state: true,
-		});
-	};
+  const allowAnswer = () => {
+    socket = io(process.env.API_URI);
+    socket.emit("set_player_buzz_state", {
+      state: true,
+    });
+  };
 
-	const disableAnswer = () => {
-		socket = io(process.env.API_URI);
-		socket.emit("set_player_buzz_state", {
-			state: false,
-		});
-	};
+  const disableAnswer = () => {
+    socket = io(process.env.API_URI);
+    socket.emit("set_player_buzz_state", {
+      state: false,
+    });
+  };
 
-	const handleResetPlayersState = () => {
-		console.log(playersJoined.concat(playersAnswered));
-		setPlayersJoined(playersJoined.concat(playersAnswered));
-		setPlayersAnswered([]);
-		allowAnswer();
-	};
+  const handleResetPlayersState = () => {
+    console.log(playersJoined.concat(playersAnswered));
+    setPlayersJoined(playersJoined.concat(playersAnswered));
+    setPlayersAnswered([]);
+    allowAnswer();
+  };
 
-	return (
-		<div className="p-10">
-			<h1 className="text-3xl font-bold mb-6">Session Admin</h1>
-			<Button variant="outline" onClick={handleRemovePlayers} className="cursor-pointer mb-4 mr-4">
-				Remove all players
-			</Button>
-			<Button variant="outline" onClick={handleResetPlayersState} className="cursor-pointer mb-4 mr-4">
-				Reset players state
-			</Button>
-			<Button onClick={allowAnswer} className="cursor-pointer mb-4 mr-4">
-				Allow Answers
-			</Button>
-			<Button onClick={disableAnswer} className="cursor-pointer mb-4 mr-4">
-				Disable Answers
-			</Button>
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-6">Session Admin</h1>
+      <Button
+        variant="outline"
+        onClick={handleRemovePlayers}
+        className="cursor-pointer mb-4 mr-4"
+      >
+        Remove all players
+      </Button>
+      <Button
+        variant="outline"
+        onClick={handleResetPlayersState}
+        className="cursor-pointer mb-4 mr-4"
+      >
+        Reset players state
+      </Button>
+      <Button onClick={allowAnswer} className="cursor-pointer mb-4 mr-4">
+        Allow Answers
+      </Button>
+      <Button onClick={disableAnswer} className="cursor-pointer mb-4 mr-4">
+        Disable Answers
+      </Button>
 
-			<div className="space-y-2 mb-6">
-				{playersAnswered.map((buzz, index) => (
-					<div key={index} className="flex justify-between bg-white p-4 shadow rounded border-l-4 border-green-500">
-						<span className="font-bold text-xl">
-							#{index + 1} {buzz.username} -- Equipe : {buzz.team} - {buzz.campus}
-						</span>
-						<span className="text-gray-500 text-sm">
-							{/* Calcul du delta temps si nécessaire, ou affichage brut */}
-							{new Date(buzz.time).toLocaleTimeString()} : {new Date(buzz.time).getMilliseconds()}
-							ms
-						</span>
-					</div>
-				))}
-			</div>
+      <div className="space-y-2 mb-6">
+        {playersAnswered.map((buzz, index) => (
+          <div
+            key={index}
+            className="flex justify-between bg-white p-4 shadow rounded border-l-4 border-green-500"
+          >
+            <span className="font-bold text-xl">
+              #{index + 1} {buzz.username} -- Equipe : {buzz.team} -{" "}
+              {buzz.campus}
+            </span>
+            <span className="text-gray-500 text-sm">
+              {/* Calcul du delta temps si nécessaire, ou affichage brut */}
+              {new Date(buzz.time).toLocaleTimeString()} :{" "}
+              {new Date(buzz.time).getMilliseconds()}
+              ms
+            </span>
+          </div>
+        ))}
+      </div>
 
-			<div className="space-y-2">
-				{playersJoined.map((buzz, index) => (
-					<div key={index} className="flex justify-between bg-white p-4 shadow rounded border-l-4 border-yellow-500">
-						<span className="font-bold text-xl">
-							#{index + 1} {buzz.username} -- Equipe : {buzz.team} - {buzz.campus}
-						</span>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+      <div className="space-y-2">
+        {playersJoined.map((buzz, index) => (
+          <div
+            key={index}
+            className="flex justify-between bg-white p-4 shadow rounded border-l-4 border-yellow-500"
+          >
+            <span className="font-bold text-xl">
+              #{index + 1} {buzz.username} -- Equipe : {buzz.team} -{" "}
+              {buzz.campus}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
